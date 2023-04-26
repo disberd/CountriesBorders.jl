83247806da499e39d8cdad86b6a29d08fe7ebcbe
Alberto Mengali <disberd@gmail.com>
2023-04-26 16:30:18 +0200

    fix dirname errro

--- src/CountriesBorders.jl
+++ src/CountriesBorders.jl
@@ -3,7 +3,7 @@ module CountriesBorders
 include("../notebooks/main_implementation.jl")
 
 function __init__()
-	shapefile = joinpath(dirname(Base.current_project()), "assets", "ne_110m_admin_0_countries_lakes.shp")
+	shapefile = joinpath(dirname(Base.current_project(@__DIR__)), "assets", "ne_110m_admin_0_countries_lakes.shp")
     GEOTABLE[] = GeoTables.load(shapefile)
 end
 
