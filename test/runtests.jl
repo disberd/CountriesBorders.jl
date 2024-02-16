996a09e720e21c6fcf93bc353cdc29538224bbf5
Alberto Mengali <disberd@gmail.com>
2024-02-16 16:10:32 +0100

    add Aqua and SafeTestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,21 @@
-using Test
+using SafeTestsets
+using Aqua
 using CountriesBorders
+Aqua.test_all(CountriesBorders; ambiguities = false)
+Aqua.test_ambiguities(CountriesBorders)
 
 
-example1 = extract_countries(;continent = "europe", admin="-russia")
-example2 = extract_countries(;admin="-russia", continent = "europe")
-example3 = extract_countries(;subregion = "*europe; -eastern europe")
-@testset "Test Docstring Examples" begin
-    @test !isnothing(example1)
-    @test !isnothing(example2)
-    @test !isnothing(example3)
-    @test length(example2) == length(example1) + 1
-    @test !isnothing(extract_countries(;ConTinEnt = "Asia"))
+@safetestset "Basic" begin
+    using CountriesBorders
+
+    example1 = extract_countries(;continent = "europe", admin="-russia")
+    example2 = extract_countries(;admin="-russia", continent = "europe")
+    example3 = extract_countries(;subregion = "*europe; -eastern europe")
+    @testset "Test Docstring Examples" begin
+        @test !isnothing(example1)
+        @test !isnothing(example2)
+        @test !isnothing(example3)
+        @test length(example2) == length(example1) + 1
+        @test !isnothing(extract_countries(;ConTinEnt = "Asia"))
+    end
 end
