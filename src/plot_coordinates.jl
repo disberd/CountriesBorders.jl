05a29c96aa2a8cebe2bb748dc7cef48590305325
Alberto Mengali <disberd@gmail.com>
2024-06-19 12:14:18 +0200

    fix extract_plot_coordinates for subdomains

--- src/plot_coordinates.jl
+++ src/plot_coordinates.jl
@@ -13,31 +13,21 @@ function extract_plot_coords(ring::Ring{2, <:SimpleLatLon})
     lon[end] = lon[1]
 	return (;lon, lat)
 end
-function extract_plot_coords(pa::PolyArea{2, <:SimpleLatLon})
-    length(rings(pa)) === 1 && return extract_plot_coords(rings(pa) |> first)
-    lat = Float32[]
-    lon = Float32[]
-    for ring ∈ rings(pa)
-        c = extract_plot_coords(ring)
-        append!(lat, c.lat)
-        append!(lon, c.lon)
-        if ring !== last(rings(pa))
-            push!(lat, NaN32)
-            push!(lon, NaN32)
-        end
-    end
-	(;lon,lat)
-end
 
-function extract_plot_coords(md::Union{Multi{2, <:SimpleLatLon}, Domain{2, <:SimpleLatLon}})
-    geoms = parent(md)
+geom_iterable(pa::PolyArea) = rings(pa)
+geom_iterable(m::Multi) = parent(m)
+geom_iterable(d::Domain) = d
+
+function extract_plot_coords(inp::Union{Multi{2, <:SimpleLatLon}, Domain{2, <:SimpleLatLon}, PolyArea{2, <:SimpleLatLon}})
+    iterable = geom_iterable(inp)
+    length(iterable) == 1 && return extract_plot_coords(first(iterable))
 	lon = Float32[]
 	lat = Float32[]
-	for geom ∈ geoms
+	for geom ∈ iterable
 		c = extract_plot_coords(geom)
         append!(lat, c.lat)
         append!(lon, c.lon)
-        if geom !== last(geoms)
+        if geom !== last(iterable)
             push!(lat, NaN32)
             push!(lon, NaN32)
         end
