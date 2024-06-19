55e48fea27beb749d8cb67344c68a9bf265fb9b2
Alberto Mengali <disberd@gmail.com>
2024-06-19 12:38:42 +0200

    add scattergeo method for vector of SimpleLatLon points

--- ext/PlotlyBaseExt.jl
+++ ext/PlotlyBaseExt.jl
@@ -1,10 +1,15 @@
 module PlotlyBaseExt
 using PlotlyBase
-using CountriesBorders: Multi, Domain, PolyArea, extract_plot_coords, SimpleLatLon
+using CountriesBorders: Multi, Domain, PolyArea, extract_plot_coords, SimpleLatLon, SimpleRegion
 
-function PlotlyBase.scattergeo(p::Union{<:Multi{2, <:SimpleLatLon}, <:Domain{2, <:SimpleLatLon}, <:PolyArea{2, <:SimpleLatLon}}; kwargs...)
+function PlotlyBase.scattergeo(p::SimpleRegion; kwargs...)
 	lon, lat = extract_plot_coords(p)
 	scattergeo(; lat, lon, mode="lines", kwargs...)
+end
+
+function PlotlyBase.scattergeo(ps::Vector{<:SimpleLatLon}; kwargs...)
+	lon, lat = extract_plot_coords(p)
+	scattergeo(; lat, lon, kwargs...)
 end
 
 end
