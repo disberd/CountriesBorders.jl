149f7a202bacb3af6428c36a2cca64ea23905030
Alberto Mengali <disberd@gmail.com>
2024-07-02 09:54:00 +0200

    up compat with CoordRefSystems and change tests to TestItemRunnner

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
-using SafeTestsets
+using TestItemRunner
 
-@safetestset "Aqua" begin
+@testitem "Aqua" begin
     using Aqua
     using CountriesBorders
     Aqua.test_all(CountriesBorders; ambiguities = false)
     Aqua.test_ambiguities(CountriesBorders)
 end
 
-@safetestset "Basic" begin include("basics.jl") end
-@safetestset "Extensions" begin include("extensions.jl") end
+@testitem "Basic" begin include("basics.jl") end
+@testitem "Extensions" begin include("extensions.jl") end
+
+@run_package_tests verbose=true
