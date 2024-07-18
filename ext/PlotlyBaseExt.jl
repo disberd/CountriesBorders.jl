1d6f88ede7f95add9ab0fd40f9edcbe94d842324
Alberto Mengali <disberd@gmail.com>
2024-07-18 17:29:11 +0200

    fix typo in scattergeo method

--- ext/PlotlyBaseExt.jl
+++ ext/PlotlyBaseExt.jl
@@ -3,7 +3,7 @@ using PlotlyBase
 using CountriesBorders: Multi, Domain, PolyArea, extract_plot_coords, SimpleLatLon, SimpleRegion
 
 function PlotlyBase.scattergeo(p::SimpleRegion; kwargs...)
-	lon, lat = extract_plot_coords(p)
+	(;lon, lat) = extract_plot_coords(p)
 	scattergeo(; lat, lon, mode="lines", kwargs...)
 end
 
